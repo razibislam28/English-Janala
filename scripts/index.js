3cec00866c69e7d5840d84c0ccdb65a7f8855017
razibislam28 <razibislam28@gmail.com>
2025-09-06 13:46:24 +0600

    added the word

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,37 @@ const loadLessons = () => {
     .then((res) => res.json()) // promise of json data
     .then((json) => displayLessons(json.data));
 };
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data));
+};
+const displayLevelWord = (words) => {
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = "";
+  words.forEach((word) => {
+    console.log(word);
+    const card = document.createElement("div");
+    card.innerHTML = `      <div
+        class="bg-whit rounded-xl shadow-sm text-center py-20 px-5 space-y-4"
+      >
+        <h2 class="text-2xl font-bold">${word.word}</h2>
+        <p class="font-semibold">Meanig /Pronounciation</p>
+        <div class="font-medium text-2xl font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+        <div class="flex justify-between items-center">
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+            <i class="fa-solid fa-circle-info"></i>
+          </button>
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+            <i class="fa-solid fa-volume-high"></i>
+          </button>
+        </div>
+      </div>`;
+    wordContainer.append(card);
+  });
+};
+
 const displayLessons = (lessons) => {
   //   1.get the container & empty
   const leveConatiner = document.getElementById("level-container");
@@ -13,7 +44,7 @@ const displayLessons = (lessons) => {
     console.log(lesson);
     const btnDiv = document.createElement("div");
 
-    btnDiv.innerHTML = `<button class="btn btn-outline btn-primary "
+    btnDiv.innerHTML = `<button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary "
                 ><i class="fa-solid fa-book-open"></i> Lesson -${lesson.level_no}</button>`;
     //   4.append into the container
     leveConatiner.appendChild(btnDiv);
